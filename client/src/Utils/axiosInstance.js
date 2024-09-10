0032a267606a1855ab5343f7b46af3af58218ea0
AJIN_VR <ajinvr44@gmail.com>
2024-09-10 18:09:33 +0530

    link change

--- client/src/Utils/axiosInstance.js
+++ client/src/Utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://bookmyturfv2.vercel.app',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 const port = process.env.PORT || 4000;
 
 const corsOptions = {
-  origin: ['https://bookmyturfv2-frontend.vercel.app', 'https://bookmyturfv2-frontend-rl3bw08pa-ajin-vrs-projects.vercel.app', 'http://192.168.1.36:5173', 'http://localhost:5173'],
+  origin: ['https://bookmyturfv2-frontend.vercel.app', 'https://vercel.com/ajin-vrs-projects/bookmyturfv2-frontend/HkaUUTYb1sZvwzBCv5FvvUHXpbGu', 'http://192.168.1.36:5173', 'http://localhost:5173'],
   credentials: true 
 };
 
