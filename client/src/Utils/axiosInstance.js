06658f4b1ac477d8f303a818c2ca192f69ade5e5
AJIN_VR <ajinvr44@gmail.com>
2024-09-14 11:47:56 +0530

    env

--- client/src/Utils/axiosInstance.js
+++ client/src/Utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
